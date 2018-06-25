955e911fe7720b441e09e54ffc0c76a6a60b40c5
김민국 <gimmingug@gimmingug-ui-macbook-pro.local>
2018-06-25 10:48:22 +0900

    Initial Commit

---
+++ GameStartTests/GameStartTests.swift
@@ -0,0 +1,36 @@
+//
+//  GameStartTests.swift
+//  GameStartTests
+//
+//  Created by 김민국 on 2018. 6. 25..
+//  Copyright © 2018년 MGHouse. All rights reserved.
+//
+
+import XCTest
+@testable import GameStart
+
+class GameStartTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
